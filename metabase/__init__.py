d1b8a48828378f48931a9ae7472f2e40d20a680a
flrngel <flrngel@gmail.com>
2017-04-14 17:29:34 +0900

    update

--- metabase/__init__.py
+++ metabase/__init__.py
@@ -1,3 +1,1 @@
-__version__ = "1.0.0"
-
 from .metabase import Metabase
--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
-import metabase
 from setuptools import setup
+
+__version__ = "1.0.1"
 
 setup(
         name="metabase",
-        version=metabase.__version__,
+        version=__version__,
         description='python wrapper for metabase api',
         url="https://github.com/stunitas/metabase-py",
         license="MIT License",
         author="flrngel",
         author_email="flrngel@gmail.com",
-        install_requires=["requests>=2.13.0"] )
+        install_requires=["requests==2.13.0"] )
